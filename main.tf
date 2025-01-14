73d6360402c23ed9a224eabd8b7b8b7a8b61ddf1
Harsh kumar <85600010+hrshkumar@users.noreply.github.com>
2025-01-14 19:39:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    apigateway = "http://localstack-main:4566"
-    lambda     = "http://localstack-main:4566"
-    iam        = "http://localstack-main:4566"
+    apigateway = "http://localhost:4566"
+    lambda     = "http://localhost:4566"
+    iam        = "http://localhost:4566"
   }
 }
 
